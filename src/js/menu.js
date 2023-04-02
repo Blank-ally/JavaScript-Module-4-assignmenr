0bf71ef2f8c9360ab3c43bfcc074653686214db5
Blank-ally <lia.stith@gmail.com>
2023-04-01 23:08:00 -0500

    init

---
+++ src/js/menu.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    $("nav").html(`
+<div class="menu">
+
+<div class="logo">
+<span class="logo-line-one"> Lia Stith Mod 4 Assignment</span>
+</div>
+<ul class="items">
+    <li>
+        <a href="/index.html">Home</a>
+    </li>
+    <li>
+    <a href="/recipe.html">Recipes</a>
+    </li>
+        
+</ul>
+</div>
+`);
+});

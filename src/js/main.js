dcd6a9558ebe414f093b833a5f6740ae8ccf5c0e
Blank-ally <lia.stith@gmail.com>
2023-04-01 23:08:09 -0500

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,61 +1,89 @@
 import { recipes } from "/src/js/data/recipe.js";
-$(function(){
- //For testing. Delete this. 
+$(function () {
+
+    for (let i = 0; i < recipes.length; i++) {
+
+        $('#cardList').append(`
+            <div>
+            <div class='card'>
+            <div class='image'><img src="${recipes[i].image}"></div>
+            <div class='content'>
+            <p>Total Time: ${recipes[i].completionTime}</p>
+                <p>${recipes[i].recipeName}</p>
+                <p>${recipes[i].category}</p>
+                <input type='button' value='More info' id="moreInfo">
+            </div>
+            </div>
+
+            <div class='card-more-info'style='display:none' >
+            <p><b>Ingredients</b></p>
+            <br>
+            <p>${recipes[i].ingredients}</p>
+            <br>
+            <p><b>Instructions</b></p>
+            <br>
+            <p>${recipes[i].description}</p>
+            </div>
+            </div>` )
+    }
 
 
- for (let i = 0; i < recipes.length; i++) {
+    $('.card').on("click", function (e) {
+        // console.log(e.target)
+        let $target = $(e.target).closest('.card')
+        $target.siblings().show('.card-more-info')
+    })
 
-    $('#cardList').append(`
-<div class='card'>
-<div class='image'><img src="${recipes[i].image}"></div>
-<div class='content'>
-<p>Total Time: ${recipes[i].completionTime}</p>
-    <p>${recipes[i].recipeName}</p>
-    <p>${recipes[i].category}</p>
-  </div>
-<div class='card-more-info hide'>
-<p>${recipes[i].description}</p>
-<p>${recipes[i].ingredients}</p>
+    $('#course').on('submit', function (e) {
+        e.preventDefault()
+        $('#cardList').html('');
+    
+        let isApp = document.getElementById('Appetizer').checked
+        let isDes = document.getElementById('Dessert').checked
+        let isEnt = document.getElementById('entree').checked
 
-</div>
-</div>
+        recipes.forEach(recipe => {
+            if (!isApp && !isDes && !isEnt) { showRecipe(recipe); }
 
-` )
+            else if ((isApp && recipe.category == 'Appetizer') || (isDes && recipe.category == 'Dessert') || (isEnt && recipe.category == 'Entrée')) {
 
-
-    //Total Time:${recipes[i].completionTime}
-    // <img src='${recipes[i].image}'>
-    // <p>${recipes[i].description}</p>
-    // <p>${recipes[i].ingredients}</p>
-}
-
-$('#cardList').on("click", function(e) {
-   
-   let $target = $(e.target).closest('.card')
-        $target.removeClass('hide')
+                showRecipe(recipe)
+            }
+        })
+        $('.card').on("click", function (e) {
+            // console.log(e.target)
+            let $target = $(e.target).closest('.card')
+            $target.siblings().show('.card-more-info')
+        })
+    })
 
 
-})
-
-$('#course').on('submit',function(e){
+        function showRecipe(recipe) {
+            $('#cardList').append(`
+        <div>
+        <div class='card'>
+        <div class='image'><img src="${recipe.image}"></div>
+        <div class='content'>
+        <p>Total Time: ${recipe.completionTime}</p>
+            <p>${recipe.recipeName}</p>
+            <p>${recipe.category}</p>
+            <input type='button' value='More info' id="moreInfo">
+        </div>
+        </div>
         
-  let isApp = document.getElementById('Appitiezer').checked
-  let isDes = document.getElementById('Dessert').checked
-  let isEnt = document.getElementById('entree').checked
-  
-  recipes.forEach(recipe => {
-    if((!isApp|| isApp && recipe.category == 'Appitiezer' )&& (!isDes || recipe.category == 'Dessert')&&(!isEnt || recipe.category == isEnt)){
-      console.log(true)
-  }});
-  
-})
-
-
-
-
-
-
-
-
-
-})
+        <div class='card-more-info'style='display:none' >
+        <p><b>Ingredients</b></p>
+        <br>
+        <p>${recipe.ingredients}</p>
+        <br>
+        <p><b>Instructions</b></p>
+        <br>
+        <p>${recipe.description}</p>
+        </div>
+        </div>
+        ` )
+        }
+    
+    
+    
+    })
